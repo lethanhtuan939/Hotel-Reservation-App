13a168af154b89f47a186e97dd353661e86669dd
amorylearcode <chosslun2k3@gmail.com>
2023-10-26 01:12:30 +0700

    addactivity

--- app/src/main/java/com/hotelreservationapp/Sign_In.java
+++ app/src/main/java/com/hotelreservationapp/Sign_In.java
@@ -2,19 +2,16 @@ package com.hotelreservationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-public class StartActivity extends AppCompatActivity {
+public class Sign_In extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
-    }
-
-    public void onButtonClick_start(View view){
         setContentView(R.layout.activity_sign_in);
     }
+
+
 }
