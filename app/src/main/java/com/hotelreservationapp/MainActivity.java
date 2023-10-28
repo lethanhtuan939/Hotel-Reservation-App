a9f778653bfa002a79bd3915623fcda8b76b8e7f
Lee Quang Luna <quangluan0212@gmail.com>
2023-10-28 09:21:04 +0700

    luanadd

--- app/src/main/java/com/hotelreservationapp/MainActivity.java
+++ app/src/main/java/com/hotelreservationapp/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent  = new Intent(getApplicationContext(),StartActivity.class);
+                Intent intent  = new Intent(getApplicationContext(),HomeActivity.class);
                 startActivity(intent);
                 finish();
             }
